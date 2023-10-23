0c5fe9555b8bd8705f6238560840e175fc5f1589
jaimeetxebarria <jaimeetxebarria@gmail.com>
2023-10-24 01:08:27 +0200

    Fix annotation inheritance behavior

--- src/main/java/org/deustomed/SQLFramework/SQLTable.java
+++ src/main/java/org/deustomed/SQLFramework/SQLTable.java
@@ -1,9 +1,10 @@
 package org.deustomed.SQLFramework;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-@Documented
-@Inherited
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SQLTable {
