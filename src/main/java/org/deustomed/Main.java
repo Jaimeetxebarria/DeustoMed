6cbe2627dd9c74e83415a05290da99e9cd365958
Jaime Etxebarria <jaimeetxebarria@gmail.com>
2023-10-27 10:49:21 +0200

    Add FlatLaf Look and Feel

--- src/main/java/org/deustomed/Main.java
+++ src/main/java/org/deustomed/Main.java
@@ -1,9 +1,11 @@
 package org.deustomed;
 
+import com.formdev.flatlaf.FlatLightLaf;
 import org.deustomed.ui.WindowLogin;
 
 public class Main {
     public static void main(String[] args) {
+        FlatLightLaf.setup();
         WindowLogin windowLogin = new WindowLogin();
         windowLogin.setSize(300, 300);
         windowLogin.setVisible(true);
