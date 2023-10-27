e56b6fbd4639277f9e4abfc2ae9d38554025ec22
Jaime Etxebarria <jaimeetxebarria@gmail.com>
2023-10-27 13:01:19 +0200

    Add Inter font

--- src/main/java/org/deustomed/Main.java
+++ src/main/java/org/deustomed/Main.java
@@ -1,11 +1,13 @@
 package org.deustomed;
 
 import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.fonts.inter.FlatInterFont;
 import org.deustomed.ui.WindowLogin;
 
 public class Main {
     public static void main(String[] args) {
         FlatLightLaf.setup();
+        FlatInterFont.install();
         WindowLogin windowLogin = new WindowLogin();
         windowLogin.setSize(300, 300);
         windowLogin.setVisible(true);
