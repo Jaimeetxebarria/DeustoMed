4c22665d06fa78e55b8269c1204329ce32b40463
AlvaroAmi <alvaro.amilibia9@gmail.com>
2023-12-08 20:55:08 +0100

    Change DB data test scripts

---
+++ generate_test_appointments.sql
@@ -0,0 +1,6 @@
+insert into appointment_type values (1,'General');
+
+insert into appointment(id, datetime, hour, reason, fk_patient_id, fk_doctor_id, fk_appointment_type_id) values
+('1', '2024-12-01', '14:30', 'Malestar general', '00AAK', '00AAH', '1'),
+('2', '2024-01-16', '18:45', 'Revision', '00AAK', '00AAJ', '1'),
+('3', '2024-03-25', '10:00', 'Vacuna', '00AAK', '00AAH', '1');
