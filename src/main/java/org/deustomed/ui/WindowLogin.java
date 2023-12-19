592c8780f6d93c579bf564a4bc5d07f0e9603621
jaimeetxebarria <jaimeetxebarria@gmail.com>
2023-12-19 13:13:10 +0100

    Change naming to apps hungarian in WindowLogin

--- src/main/java/org/deustomed/ui/WindowLogin.java
+++ src/main/java/org/deustomed/ui/WindowLogin.java
@@ -2,48 +2,44 @@ package org.deustomed.ui;
 
 
 import javax.swing.*;
-import javax.swing.text.PasswordView;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Arrays;
-import java.util.Date;
 
 public class WindowLogin extends JFrame {
-    JLabel loginLbl;
-    JLabel emailLbl;
-    JLabel passwordLbl;
-    JLabel errorLbl;
-    JTextField emailTf;
-    JPasswordField passwordTf;
-    JButton loginBtn;
+    JLabel lblLogin;
+    JLabel lblEmail;
+    JLabel lblPassword;
+    JLabel lblError;
+    JTextField txtEmail;
+    JPasswordField txtPassword;
+    JButton btnLogin;
 
     public WindowLogin(){
-        loginLbl = new JLabel("Login");
-        emailLbl = new JLabel("Email:");
-        passwordLbl = new JLabel("Clave:");
-        emailTf = new JTextField();
-        passwordTf = new JPasswordField();
-        loginBtn = new JButton("Login");
-        errorLbl = new JLabel("");
+        lblLogin = new JLabel("Login");
+        lblEmail = new JLabel("Email:");
+        lblPassword = new JLabel("Clave:");
+        txtEmail = new JTextField();
+        txtPassword = new JPasswordField();
+        btnLogin = new JButton("Login");
+        lblError = new JLabel("");
 
-        loginLbl.setFont(loginLbl.getFont().deriveFont(20f));
-        errorLbl.setForeground(java.awt.Color.RED);
+        lblLogin.setFont(lblLogin.getFont().deriveFont(20f));
+        lblError.setForeground(java.awt.Color.RED);
 
-        loginLbl.setBounds(115,20,100,30);
-        emailLbl.setBounds(45,70,100,30);
-        passwordLbl.setBounds(45,130,100,30);
-        emailTf.setBounds(45,100,200,30);
-        passwordTf.setBounds(45,160,200,30);
-        loginBtn.setBounds(95,220,100,30);
-        errorLbl.setBounds(15,270,250,30);
+        lblLogin.setBounds(115, 20, 100, 30);
+        lblEmail.setBounds(45, 70, 100, 30);
+        lblPassword.setBounds(45, 130, 100, 30);
+        txtEmail.setBounds(45, 100, 200, 30);
+        txtPassword.setBounds(45, 160, 200, 30);
+        btnLogin.setBounds(95, 220, 100, 30);
+        lblError.setBounds(15, 270, 250, 30);
 
-        add(loginLbl);
-        add(emailLbl);
-        add(passwordLbl);
-        add(emailTf);
-        add(passwordTf);
-        add(loginBtn);
-        add(errorLbl);
+        add(lblLogin);
+        add(lblEmail);
+        add(lblPassword);
+        add(txtEmail);
+        add(txtPassword);
+        add(btnLogin);
+        add(lblError);
 
         setLayout(null);
         this.setResizable(false);
@@ -54,9 +50,9 @@ public class WindowLogin extends JFrame {
         setVisible(true);
 
 
-        loginBtn.addActionListener(e -> {
-            String email = emailTf.getText();
-            char[] passwordChars = passwordTf.getPassword();
+        btnLogin.addActionListener(e -> {
+            String email = txtEmail.getText();
+            char[] passwordChars = txtPassword.getPassword();
             String password = new String(passwordChars);
             Arrays.fill(passwordChars, ' '); //Borrar la contraseña de la memoria
             if (loginChecker(email,password)) {
@@ -73,22 +69,22 @@ public class WindowLogin extends JFrame {
      */
     public boolean loginChecker(String email, String password){
         if(email.isEmpty()){
-            errorLbl.setText("Error: El email no puede estar vacio");
+            lblError.setText("Error: El email no puede estar vacio");
             return false;
         } else if(email.length() < 5){
-            errorLbl.setText("Error: El email debe tener al menos 5 caracteres");
+            lblError.setText("Error: El email debe tener al menos 5 caracteres");
             return false;
         } else if(!email.contains("@")){
-            errorLbl.setText("Error: El email debe contener un @");
+            lblError.setText("Error: El email debe contener un @");
             return false;
         } else if (password.isEmpty()){
-            errorLbl.setText("Error: La contraseña no puede estar vacia");
+            lblError.setText("Error: La contraseña no puede estar vacia");
             return false;
         } else if (password.length() < 8){
-            errorLbl.setText("Error: La contraseña debe tener al menos 8 caracteres");
+            lblError.setText("Error: La contraseña debe tener al menos 8 caracteres");
             return false;
         } else {
-            errorLbl.setText("");
+            lblError.setText("");
             return true;
         }
     }
