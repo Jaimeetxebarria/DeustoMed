1647157967fde99c8b0e4870d8f5ae66a14361b2
jaimeetxebarria <jaimeetxebarria@gmail.com>
2024-01-01 21:32:06 +0100

    Fix windows not logging out if dispose() is called

--- src/main/java/org/deustomed/ui/UserAuthenticatedWindow.java
+++ src/main/java/org/deustomed/ui/UserAuthenticatedWindow.java
@@ -9,17 +9,28 @@ import java.awt.event.WindowAdapter;
  * Used to log out from the UserAuthenticationService when the window is closed
  */
 public class UserAuthenticatedWindow extends JFrame {
+    UserAuthenticationService userAuthenticationService;
     public UserAuthenticatedWindow(UserAuthenticationService userAuthenticationService) {
         if (userAuthenticationService == null) return;
+        this.userAuthenticationService = userAuthenticationService;
         addWindowListener(new WindowAdapter() {
             @Override
+            public void windowClosed(java.awt.event.WindowEvent windowEvent) {
+                logOut();
+            }
+
+            @Override
             public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-                try {
-                    userAuthenticationService.close();
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
+                logOut();
             }
         });
+    }
+
+    private void logOut() {
+        try {
+            userAuthenticationService.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
