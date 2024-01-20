5d71197ada676e0bdfebf08665e53d8762810ba8
jaimeetxebarria <jaimeetxebarria@gmail.com>
2024-01-20 20:31:59 +0100

    Fix WindowAdmin row filter error when using '+'

--- src/main/java/org/deustomed/ui/WindowAdmin.java
+++ src/main/java/org/deustomed/ui/WindowAdmin.java
@@ -520,6 +520,7 @@ public class WindowAdmin extends UserAuthenticatedWindow {
         if (filter.trim().length() == 0) {
             rowSorter.setRowFilter(null);
         } else {
+            filter = filter.replace("+", "\\+");
             rowSorter.setRowFilter(RowFilter.regexFilter("(?i)^" + filter));
         }
     }
