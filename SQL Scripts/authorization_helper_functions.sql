6923ae0ce4b9f86b646eb6a2a718d58cddc162ae
jaimeetxebarria <jaimeetxebarria@gmail.com>
2024-01-21 19:39:14 +0100

    Fix custom_auth.user_id(): return text instead of uuid

--- SQL Scripts/authorization_helper_functions.sql
+++ SQL Scripts/authorization_helper_functions.sql
@@ -1,5 +1,5 @@
 create or replace function custom_auth.user_id() returns uuid as $$
-  select nullif(current_setting('custom_auth.user_id', true), '')::uuid;
+  select nullif(current_setting('custom_auth.user_id', true), '')::text;
 $$ language sql stable;
 
 create or replace function custom_auth.user_type() returns text as $$
