c4ec4b58be264b8b880bf44203d383a4f54e29f8
AlvaroAmi <alvaro.amilibia9@gmail.com>
2024-01-21 19:59:35 +0100

    Small SQL changes

--- SQL Scripts/generate_test_appointments.sql
+++ SQL Scripts/generate_test_appointments.sql
@@ -1,9 +1,9 @@
 insert into appointment_type values (1,'General');
 
 insert into appointment (reason, fk_patient_id, fk_doctor_id, fk_appointment_type_id, date) values
-    ('Malestar general', '00AAK', '00AAH', 1, '2024-12-01 14:30'),
-    ('Revisión', '00AAK', '00AAJ', 1, '2024-12-01 14:30'),
-    ('Vacuna', '00AAK', '00AAH', 1, '2024-12-01 14:30'),
+  ('Malestar general', '00AAK', '00AAH', 1, '2024-01-25 14:30'),
+    ('Revisión', '00AAK', '00AAJ', 1, '2024-02-02 14:30'),
+    ('Vacuna', '00AAK', '00AAH', 1, '2024-02-16 14:30'),
     ('Dolor abdominal', '00ABA', '00AAG', 1, '2024-01-22T12:30:00Z'),
     ('Nauseas severas', '00ABB', '00AAG', 1, '2024-01-22T13:00:00Z'),
     ('Dolor muscular en la rodilla', '00ABC', '00AAG', 1, '2024-01-22T13:30:00Z'),
