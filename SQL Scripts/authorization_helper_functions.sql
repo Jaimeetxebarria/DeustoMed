fceca2438912e149b0f7d5683fb370bd72ee1a7c
jaimeetxebarria <jaimeetxebarria@gmail.com>
2024-01-21 23:53:04 +0100

    Fix typo in auth_helper_functions

--- SQL Scripts/authorization_helper_functions.sql
+++ SQL Scripts/authorization_helper_functions.sql
@@ -3,5 +3,5 @@ create or replace function custom_auth.user_id() returns uuid as $$
 $$ language sql stable;
 
 create or replace function custom_auth.user_type() returns text as $$
-  select nullif(current_setting('custom_auth.user_id', true), '')::text;
+  select nullif(current_setting('custom_auth.user_type', true), '')::text;
 $$ language sql stable;
