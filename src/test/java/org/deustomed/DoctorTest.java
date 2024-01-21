e32bc31a1e5b339e2447cb40c3aca4ce7c5d3eb1
jaimeetxebarria <jaimeetxebarria@gmail.com>
2024-01-21 20:34:30 +0100

    Fix DoctorTest: update appointment list

--- src/test/java/org/deustomed/DoctorTest.java
+++ src/test/java/org/deustomed/DoctorTest.java
@@ -5,7 +5,9 @@ import org.deustomed.postgrest.PostgrestClientFactory;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,9 +18,18 @@ class DoctorTest {
         PostgrestClient postgrestClient = PostgrestClientFactory.createSuperuserClient();
         Doctor doctor = new Doctor("00AAA", postgrestClient);
 
+        List<Appointment> expectedAppointments = List.of(
+                new Appointment("00ABG", "00AAA",
+                        LocalDateTime.of(2024, 1, 22, 14, 30, 0),
+                        "Posible choque anafiláctico leve", "General"),
+                new Appointment("00ABH", "00AAA",
+                        LocalDateTime.of(2024, 1, 22, 14, 45, 0),
+                        "Síntomas de alergia", "General")
+        );
+
         Doctor expectedDoctor = new Doctor("00AAA", "Elena", "García", "Martínez",
-                LocalDate.parse("1990-05-15"), Sex.FEMALE, "12345678A", "elena.garcia@email.com",
-                "+123456789", "Street 123, City", "Alergología", new ArrayList<>());
+                LocalDate.parse("1990-05-15"), Sex.FEMALE, "12345679A", "elena.garcia@email.com",
+                "623456789", "Street 123, City", "Alergología", new ArrayList<>(expectedAppointments));
 
         assertEquals(expectedDoctor, doctor);
     }
